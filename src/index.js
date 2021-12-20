0bf1aeb4630eb6ed3ed05e0d1591cad1f4ff0ba1
Jonatan Gomez <jgp0007@alu.medac.es>
2021-12-20 11:47:08 +0100

    peticion get hecha y post solucionar

--- src/index.js
+++ src/index.js
@@ -1,34 +1,3 @@
-const { query } = require('express');
-const express = require('express');
-const app = express();
-const morgan=require('morgan');
- 
-//Configuraciones
-app.set('port', process.env.PORT || 3000);
-app.set('json spaces', 2)
- 
-//Middleware
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
- 
-//Nuestro primer WS Get
-app.get('/', (req, res) => {    
-  let sql = "SELECT * from datos_usuario where id_usuario = 1";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      res.json(result);
-      console.log("Result: " + JSON.stringify(result,null,2));
-    });
-})
- 
-//Iniciando el servidor
-app.listen(app.get('port'),()=>{
-    console.log(`Server listening on port ${app.get('port')}`);
-});
-
-//Routes
-app.use(require('./routes/index'));
 
 //MySQL
 var mysql = require('mysql');
@@ -44,5 +13,67 @@ var con = mysql.createConnection({
     if (err) throw err;
     console.log("Connected!");
   });
+
+
+const { query } = require('express');
+const express = require('express');
+const app = express();
+const morgan=require('morgan');
+ 
+//Configuraciones
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2)
+ 
+//Middleware
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+ 
+//Nuestro primer WS Get
+app.get('/usuario', (req, res) => {    
+  const id_usuario = req.query.id_usuario;
+  let sql = "SELECT * from datos_usuario where id_usuario =" + id_usuario;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      res.json(result);
+      // console.log("Result: " + JSON.stringify(result,null,2));
+    });
+})
+
+app.get('/vehiculos', (req, res) => {    
+  const id_usuario = req.query.id_usuario;
+  let sql = "SELECT * from lista_vehiculos where id_usuario =" + id_usuario;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      res.json(result);
+      // console.log("Result: " + JSON.stringify(result,null,2));
+    });
+})
+ 
+
+//Nuestro primer WS POST
+app.post('/editUsuario', (req, res) => {    
+  const id_usu = req.body.id_usu;
+  const name = req.body.nombre;
+
+  const id_usuario = req.query.id_usuario;
+  let sql = "insert into tallerservidores(nombre) values("+name+"))";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      // res.json(result);
+      // console.log("Result: " + JSON.stringify(result,null,2));
+    });
+})
+
+
+
+//Iniciando el servidor
+app.listen(app.get('port'),()=>{
+    console.log(`Server listening on port ${app.get('port')}`);
+});
+
+//Routes
+// app.use(require('./routes/index'));
+
 
   
